e12ce45f388cbb40a6f18450500ed78cee6f4cdb
Tatibnomb <49379010@est.ort.edu.ar>
2025-09-17 08:53:33 -0300

    Raycast detecta un gameObject y muestra su nombre en consola

---
+++ Assets/RaycastVision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastVision : MonoBehaviour
+{
+    [SerializeField] Vector3 rayOrigin;
+    [SerializeField] Transform sightOrigin;
+    [SerializeField] float rayDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(sightOrigin.position, sightOrigin.forward, out hitInfo, rayDistance))
+        {
+            Debug.Log(hitInfo.collider.gameObject);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Color color = Color.red;
+        Gizmos.color = color;
+        Gizmos.DrawLine(sightOrigin.position, sightOrigin.position + sightOrigin.forward * rayDistance);
+    }
+}
